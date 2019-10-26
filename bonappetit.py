54b9917f1ae3e59db6a83d9981b3cc31cadf0073
KatarinaMel <kbmelend@ucsd.edu>
2019-10-26 07:21:11 -0700

    Adding the basic python web template

---
+++ bonappetit.py
@@ -0,0 +1,11 @@
+# File to create the bon-APP-etit website
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return render_template('home.html')
+    
+if __name__ == '__main__':
+  app.run(debug=True)
